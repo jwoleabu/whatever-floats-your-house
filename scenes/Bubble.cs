f3dc5836703625c322f291a96666dae3b9370b20
Joseph Wole-Abu <jwoleabu@gmail.com>
2025-01-26 14:22:42 +0000

    house physics

--- scenes/Bubble.cs
+++ scenes/Bubble.cs
@@ -1,21 +1,46 @@
 using Godot;
 using System;
+using System.Diagnostics;
 
 public partial class Bubble : RigidBody2D
 {
     private DampedSpringJoint2D _springJoint;
     private RigidBody2D player;
+    private Node2D nodeA;
+    private Node2D nodeB;
 
     public override void _Ready()
     {
         player = GetNode<RigidBody2D>("/root/Joscene/House");
-        GD.Print(player.ToString());
         _springJoint = GetNode<DampedSpringJoint2D>("DampedSpringJoint2D");
-        ConnectToHouse();
+    }
+
+    public void SetNodeAB(Node2D nodeA, Node2D nodeB)
+    {
+        this.nodeA = nodeA;
+        this.nodeB = nodeB;
+    }
+
+    public override void _Draw()
+    {
+        if (nodeA == null|| nodeB == null)
+        {
+            return;
+        }
+        DrawLine(nodeA.Position, nodeB.Position, Colors.Red, 2f);
+    }
+
+    public override void _Process(double delta)
+    {
+        QueueRedraw();
     }
 
     public void ConnectToHouse()
     {
         _springJoint.NodeB = player.GetPath();
+    }
+    public void ConnectToHouse(NodePath path)
+    {
+        _springJoint.NodeB = path;
     }
 }
