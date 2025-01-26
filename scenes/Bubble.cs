1a339fcae61e1c5861e17864f163e9eee718061f
Akshay Gopinath <akshay8055.007@gmail.com>
2025-01-26 15:48:01 +0000

    shake with sprite

--- scenes/Bubble.cs
+++ scenes/Bubble.cs
@@ -4,44 +4,44 @@ using System.Diagnostics;
 
 public partial class Bubble : RigidBody2D
 {
-    private DampedSpringJoint2D _springJoint;
-    private RigidBody2D player;
-    private Node2D nodeA;
-    private Node2D nodeB;
+	private DampedSpringJoint2D _springJoint;
+	private RigidBody2D player;
+	private Node2D nodeA;
+	private Node2D nodeB;
 
-    public override void _Ready()
-    {
-        player = GetNode<RigidBody2D>("/root/Joscene/House");
-        _springJoint = GetNode<DampedSpringJoint2D>("DampedSpringJoint2D");
-    }
+	public override void _Ready()
+	{
+		player = GetNode<RigidBody2D>("/root/Joscene/House");
+		_springJoint = GetNode<DampedSpringJoint2D>("DampedSpringJoint2D");
+	}
 
-    public void SetNodeAB(Node2D nodeA, Node2D nodeB)
-    {
-        this.nodeA = nodeA;
-        this.nodeB = nodeB;
-    }
+	public void SetNodeAB(Node2D nodeA, Node2D nodeB)
+	{
+		this.nodeA = nodeA;
+		this.nodeB = nodeB;
+	}
 
-    public override void _Draw()
-    {
-        if (nodeA == null|| nodeB == null)
-        {
-            return;
-        }
-        DrawLine(nodeA.Position, nodeB.Position, Colors.White, 2f);
-    }
+	public override void _Draw()
+	{
+		if (nodeA == null|| nodeB == null)
+		{
+			return;
+		}
+		DrawLine(nodeA.Position, nodeB.Position, Colors.White, 2f);
+	}
 
-    public override void _PhysicsProcess(double delta)
-    {
-        QueueRedraw();
-    }
+	public override void _PhysicsProcess(double delta)
+	{
+		QueueRedraw();
+	}
 
 
-    public void ConnectToHouse()
-    {
-        _springJoint.NodeB = player.GetPath();
-    }
-    public void ConnectToHouse(NodePath path)
-    {
-        _springJoint.NodeB = path;
-    }
+	public void ConnectToHouse()
+	{
+		_springJoint.NodeB = player.GetPath();
+	}
+	public void ConnectToHouse(NodePath path)
+	{
+		_springJoint.NodeB = path;
+	}
 }
